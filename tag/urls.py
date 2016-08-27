2cbdcf4087e15dad593bcf0ce618f5136eeafd3e
bernard <barjebernard@gmail.com>
2016-08-26 22:44:57 -0400

    fixed url

--- tag/urls.py
+++ tag/urls.py
@@ -3,7 +3,7 @@ from .views import TagListView, CreateTagView, TagDetailView
 
 
 urlpatterns = [
-    url(r'^', CreateTagView.as_view(), name='list'),
-    url(r'^add/$', CreateTagView.as_view(), name='add'),
+    url(r'^$', CreateTagView.as_view(), name='list'),
+    url(r'^add/$', TagListView.as_view(), name='add'),
     url(r'^(?P<pk>[0-9]+)/$', TagDetailView.as_view(), name='detail'),
 ]
